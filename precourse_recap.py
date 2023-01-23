0d053197133c073a4bbf8c9065c25d8b75ac45c7
SShingler <s.shingler@hotmail.co.uk>
2023-01-23 18:20:32 +0000

    second commit - rounds age in days & weeks to whole number

--- precourse_recap.py
+++ precourse_recap.py
@@ -4,5 +4,5 @@ age_days = age_years * 365
 age_weeks = age_days / 7
 
 print("Hello "+ name + ", you are " + str(age_years) + " years old.")
-print("That is equal to " + str(age_days) + " days, or " + str(age_weeks) + " weeks.")
+print("That is equal to " + str(round(age_days)) + " days, or " + str(round(age_weeks)) + " weeks.")
 
